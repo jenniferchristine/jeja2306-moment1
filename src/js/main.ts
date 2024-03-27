87169582cea1d6836fa05533715526497094d99a
jenniferchristine <jeja2306@student.miun.se>
2024-03-27 17:36:16 +0100

    small changes

--- src/js/main.ts
+++ src/js/main.ts
@@ -5,42 +5,55 @@ interface CourseInfo { // interface för objekt
     syllabus: string;
 }
 
+window.addEventListener('load', () => { // ladda kurser vid reload
+    loadResult();
+});
+
 const addCourseBtn = document.getElementById('addCourse') as HTMLButtonElement;
 
-function saveResult(course: CourseInfo) { // spara till local storage
-    const courses = JSON.parse(localStorage.getItem('courses') || '[]'); // hämtar data från local storage
+function saveResult(course: CourseInfo) : void { // spara till local storage
+    const courses: CourseInfo[] = JSON.parse(localStorage.getItem('courses') || '[]'); // hämtar data från local storage
     courses.push(course);
     localStorage.setItem('courses', JSON.stringify(courses)); // konverterar
 }
 
-function loadResult() { // ladda tillägg
-    const courses = JSON.parse(localStorage.getItem('courses') || '[]');
+function loadResult() : void { // ladda tillägg
+    const courses: CourseInfo[] = JSON.parse(localStorage.getItem('courses') || '[]');
     courses.forEach((course: CourseInfo) => { // loopar igenom result
         displayResult(course); // anropa för visning
     });
 }
 
-function displayResult(course: CourseInfo) { // visa kurs
+function displayResult(course: CourseInfo) : void { // visa kurs
     const container = document.getElementById('resultContainer') as HTMLDivElement;
-    const resultDiv = document.createElement('div') as HTMLDivElement;
+    const resultDiv: HTMLDivElement = document.createElement('div');
     resultDiv.classList.add('result');
     resultDiv.innerHTML = `Kurskod: ${course.code}<br>Kursnamn: ${course.name}<br>Progression: ${course.progression}<br>URL: ${course.syllabus}`;
 
-    const clearResultBtn = document.createElement('button');
-    clearResultBtn.classList.add('eraseResult');
-    clearResultBtn.textContent = 'Ta bort kurs';
+    const clearResultBtn: HTMLButtonElement = document.createElement('button');
+    clearResultBtn.classList.add('resultBtn');
+    clearResultBtn.textContent = "Ta bort kurs";
 
     clearResultBtn.addEventListener('click', () => {
         container.removeChild(resultDiv); // tar bort tillhörande result
         removeResult(course);
     });
 
+    const editBtn: HTMLButtonElement = document.createElement('button');
+    editBtn.classList.add('resultBtn');
+    editBtn.textContent = "Redigera kurs";
+
+    editBtn.addEventListener('click', () => {
+        editResult(course);
+    });
+
+    resultDiv.appendChild(editBtn);
     resultDiv.appendChild(clearResultBtn);
     container.appendChild(resultDiv);
 }
 
-function removeResult(course: CourseInfo) { // ta bort
-    let courses = JSON.parse(localStorage.getItem('courses') || '[]');
+function removeResult(course: CourseInfo) : void { // ta bort
+    let courses: CourseInfo[] = JSON.parse(localStorage.getItem('courses') || '[]');
     courses = courses.filter((c: CourseInfo) => c.code !== course.code); // behåller övriga result genom filtrering och tar bort aktuell
     localStorage.setItem('courses', JSON.stringify(courses));
 }
@@ -51,10 +64,10 @@ addCourseBtn.addEventListener('click', () => { // lägga till kurs
     const syllabusCondition = (document.getElementById('syllabus') as HTMLInputElement);
     const progressionCondition = (document.getElementById('progression') as HTMLInputElement); // hämtar input
 
-    const code = codeCondition.value;
-    const name = nameCondition.value;
-    const syllabus = syllabusCondition.value;
-    const progression = progressionCondition.value.toUpperCase(); // stora eller små bokstäver
+    const code: string = codeCondition.value;
+    const name: string = nameCondition.value;
+    const syllabus: string = syllabusCondition.value;
+    const progression: string = progressionCondition.value.toUpperCase(); // stora eller små bokstäver
 
     if (!["A", "B", "C"].includes(progression)) { // kontroll för bokstäver
         alert("OBS: Du kan endast ange progression A, B eller C");
@@ -81,8 +94,4 @@ addCourseBtn.addEventListener('click', () => { // lägga till kurs
 
     saveResult(course); // spara i local storage
     displayResult(course); // visa resultat
-});
-
-window.addEventListener('load', () => { // ladda kurser vid reload
-    loadResult();
 });
